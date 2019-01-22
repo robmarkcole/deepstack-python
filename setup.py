41059e8bb35543e389130b5490b73ba9a1434230
robmarkcole <robmarkcole@gmail.com>
2019-01-22 05:29:57 +0000

    Initial

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.1"
+
+REQUIRES = ["requests"]
+
+setup(
+    name="deepstack-python",
+    version=VERSION,
+    url="https://github.com/robmarkcole/deepstack-python",
+    author="Robin Cole",
+    author_email="robmarkcole@gmail.com",
+    description="Unofficial python API for DeepStack",
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    license="Apache License, Version 2.0",
+    python_requires=">=3.5",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+)
