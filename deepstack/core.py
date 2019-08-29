6f6aa17579a361405fcb0044f5429f6bd12790f0
Robin Cole <robmarkcole@gmail.com>
2019-08-29 05:30:00 +0100

    Simplify

--- deepstack/core.py
+++ deepstack/core.py
@@ -1,12 +1,10 @@
 """
 Deepstack core.
 """
-import imghdr
 import requests
 from PIL import Image
 
 ## Const
-CLASSIFIER = "deepstack"
 HTTP_OK = 200
 HTTP_BAD_REQUEST = 400
 HTTP_UNAUTHORIZED = 401
@@ -17,45 +15,19 @@ def get_matched_faces(predictions: dict):
     """
     Get the predicted faces and their confidence.
     """
-    try:
-        matched_faces = {
-            face["userid"]: round(face["confidence"] * 100, 1)
-            for face in predictions
-            if not face["userid"] == "unknown"
-        }
-        return matched_faces
-    except:
-        return {}
-
-
-def is_valid_image(file_path: str):
-    """
-    Check file_path is valid image, using PIL then imghdr.
-    """
-    try:
-        with Image.open(file_path):
-            pass
-
-        image_extension = imghdr.what(file_path)
-        if image_extension in ["jpeg", ".jpg", ".png"]:
-            return True
-        return False
-    except Exception as exc:
-        print(exc)
-        return False
+    matched_faces = {}
+    matched_faces = {
+        face["userid"]: round(face["confidence"] * 100, 1)
+        for face in predictions
+        if not face["userid"] == "unknown"
+    }
+    return matched_faces
 
 
 def post_image(url: str, image: bytes):
     """Post an image to the classifier."""
-    try:
-        response = requests.post(url, files={"image": image}, timeout=TIMEOUT)
-        return response
-    except requests.exceptions.ConnectionError:
-        print("ConnectionError: Is %s running?", CLASSIFIER)
-        return None
-    except requests.exceptions.Timeout:
-        print("Timeout error from %s", CLASSIFIER)
-        return None
+    response = requests.post(url, files={"image": image}, timeout=TIMEOUT)
+    return response
 
 
 class DeepstackFace:
@@ -72,9 +44,8 @@ class DeepstackFace:
 
     def process_file(self, file_path: str):
         """Process an image file."""
-        if is_valid_image(file_path):
-            with open(file_path, "rb") as image_bytes:
-                self.process_image_bytes(image_bytes)
+        with open(file_path, "rb") as image_bytes:
+            self.process_image_bytes(image_bytes)
 
     def process_image_bytes(self, image_bytes: bytes):
         """Process an image."""
