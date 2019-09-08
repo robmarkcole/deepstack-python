5999a21d2a6ef5f271d1bc7d5eb80da26a57fef2
Robin Cole <robmarkcole@gmail.com>
2019-09-08 14:26:56 +0100

    Update test

--- tests/test_deepstack.py
+++ tests/test_deepstack.py
@@ -1,7 +1,9 @@
 # Placeholder
 
 import deepstack.core as ds
+import requests
 import requests_mock
+import pytest
 
 MOCK_IP_ADDRESS = "localhost"
 MOCK_PORT = 5000
@@ -46,13 +48,25 @@ MOCK_CONFIDENCES = [0.6998661, 0.7996547]
 CONFIDENCE_THRESHOLD = 0.7
 
 
-def test_DeepstackObject():
+def test_DeepstackObject_process_image_bytes():
+    """Test a good response from server."""
     with requests_mock.Mocker() as mock_req:
         mock_req.post(MOCK_URL, status_code=ds.HTTP_OK, json=MOCK_RESPONSE)
 
         dsobject = ds.DeepstackObject(MOCK_IP_ADDRESS, MOCK_PORT)
         dsobject.process_image_bytes(MOCK_BYTES)
         assert dsobject.predictions == MOCK_PREDICTIONS
+
+
+def test_DeepstackObject_process_image_bytes_timeout():
+    """Test a timeout. THIS SHOULD FAIL"""
+    with pytest.raises(ds.DeepstackException) as excinfo:
+        with requests_mock.Mocker() as mock_req:
+            mock_req.post(MOCK_URL, exc=requests.exceptions.ConnectTimeout)
+            dsobject = ds.DeepstackObject(MOCK_IP_ADDRESS, MOCK_PORT)
+            dsobject.process_image_bytes(MOCK_BYTES)
+            assert False
+            assert "SHOULD FAIL" in str(excinfo.value)
 
 
 def test_get_object_labels():
