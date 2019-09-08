f038eb7461f2a27eb5fc9afca1215074ce7ec8a8
Robin Cole <robmarkcole@gmail.com>
2019-09-08 13:36:28 +0100

    Test functions

--- tests/test_deepstack.py
+++ tests/test_deepstack.py
@@ -33,10 +33,26 @@ MOCK_RESPONSE = {
 }
 
 MOCK_PREDICTIONS = MOCK_RESPONSE["predictions"]
+MOCK_CONFIDENCES = [0.6998661, 0.7996547]
+CONFIDENCE_THRESHOLD = 0.7
 
 
 def test_get_object_labels():
+    """Cant always be sure order of returned list items."""
     object_labels = ds.get_object_labels(MOCK_PREDICTIONS)
+    assert type(object_labels) is list
     assert "dog" in object_labels
     assert "person" in object_labels
     assert len(object_labels) == 2
+
+def test_get_objects_summary():
+    objects_summary = ds.get_objects_summary(MOCK_PREDICTIONS)
+    assert objects_summary == {'dog': 1, 'person': 2}
+
+def test_get_label_confidences():
+    label_confidences = ds.get_label_confidences(MOCK_PREDICTIONS, 'person')
+    assert label_confidences == MOCK_CONFIDENCES
+
+
+def test_get_confidences_above_threshold():
+    assert len(ds.get_confidences_above_threshold(MOCK_CONFIDENCES, CONFIDENCE_THRESHOLD)) == 1
